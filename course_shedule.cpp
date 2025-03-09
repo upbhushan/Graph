8ae20afe403bdd126316d6307f4436052ff09d73
upbhushan <upbhushan217@gmail.com>
2025-03-09 23:52:46 +0530

    ...

--- course_shedule.cpp
+++ course_shedule.cpp
@@ -1,89 +1,90 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
 
 
-// question 1
-class solution{
-    public:
-    bool is_possible(int n,vector<vector<int>> &mat){
-        vector<vector<int>> adj(n);
-        for(auto ele:mat){
-            adj[ele[1]].push_back(ele[0]);
-        }
+// // question 1
+// class solution{
+//     public:
+//     bool is_possible(int n,vector<vector<int>> &mat){
+//         vector<vector<int>> adj(n);
+//         for(auto ele:mat){
+//             adj[ele[1]].push_back(ele[0]);
+//         }
 
-        vector<int> indeg(n,0);
+//         vector<int> indeg(n,0);
 
-        for(int i=0;i<n;i++){
-            for(auto node:adj[i]){
-                indeg[node]++;
-            }
-        }
+//         for(int i=0;i<n;i++){
+//             for(auto node:adj[i]){
+//                 indeg[node]++;
+//             }
+//         }
 
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0){
-                q.push(i);
-            }
-        }
+//         queue<int> q;
+//         for(int i=0;i<n;i++){
+//             if(indeg[i]==0){
+//                 q.push(i);
+//             }
+//         }
 
-        int cnt=0;
+//         int cnt=0;
 
-        while(!q.empty()){
-            auto node=q.front();
-            q.pop();
-            cnt++;
+//         while(!q.empty()){
+//             auto node=q.front();
+//             q.pop();
+//             cnt++;
 
-            for(auto ele:adj[node]){
-                indeg[ele]--;
-                if(indeg[ele]==0){
-                    q.push(ele);
-                }
-            }
-        }
-        return cnt==n;
-    }
-};
+//             for(auto ele:adj[node]){
+//                 indeg[ele]--;
+//                 if(indeg[ele]==0){
+//                     q.push(ele);
+//                 }
+//             }
+//         }
+//         return cnt==n;
+//     }
+// };
 
 
-//question 2
+// //question 2
 
-class solution{
-    public:
+// class solution{
+//     public:
 
-    vector<int> course_shedule(int n,vector<vector<int>> &mat){
-        vector<vector<int>> adj(n);
-        vector<int> indeg(n,0);
-        for(auto ele:mat){
-            adj[ele[1]].push_back(ele[0]);
-            indeg[ele[0]]++;
-        }
+//     vector<int> course_shedule(int n,vector<vector<int>> &mat){
+//         vector<vector<int>> adj(n);
+//         vector<int> indeg(n,0);
+//         for(auto ele:mat){
+//             adj[ele[1]].push_back(ele[0]);
+//             indeg[ele[0]]++;
+//         }
 
-        vector<int> ans;
+//         vector<int> ans;
 
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indeg[i]==0) {
-                q.push(i);
-            }
-        }
+//         queue<int> q;
+//         for(int i=0;i<n;i++){
+//             if(indeg[i]==0) {
+//                 q.push(i);
+//             }
+//         }
 
-        while(!q.empty()){
-            auto node=q.front();
-            q.pop();
+//         while(!q.empty()){
+//             auto node=q.front();
+//             q.pop();
 
-            ans.push_back(node);
+//             ans.push_back(node);
 
-            for(auto ele:adj[node]){
-                indeg[ele]--;
-                if(indeg[ele]==0){
-                    q.push(ele);
-                }
-            }
-        }
+//             for(auto ele:adj[node]){
+//                 indeg[ele]--;
+//                 if(indeg[ele]==0){
+//                     q.push(ele);
+//                 }
+//             }
+//         }
 
-        if(ans.size()==n) return ans;
-        else return {};
+//         if(ans.size()==n) return ans;
+//         else return {};
 
-    }
-};
+//     }
+// };
+
